2e8bc700447dfa264858788e148e40e3c0bcbc5e
exnfachjan <jan.baureis@icloud.com>
2026-04-08 13:34:41 +0200

    fix

--- src/main/java/me/exnfachjan/twitchRandomizer/gui/GuiListener.java
+++ src/main/java/me/exnfachjan/twitchRandomizer/gui/GuiListener.java
@@ -100,7 +100,7 @@ public class GuiListener implements Listener {
                     p.sendMessage(ChatColor.RED + "Keine Berechtigung: twitchrandomizer.timer.stop");
                     return;
                 }
-                try { plugin.getTimerManager().pause(); } catch (Throwable ignored) {}
+                try { plugin.getTimerManager().stop(); } catch (Throwable ignored) {}
                 refreshMenu(type, p);
             }
             case "timer_reset" -> {
@@ -230,7 +230,7 @@ public class GuiListener implements Listener {
                 } else if (e.isRightClick()) {
                     plugin.getDeathCounter().increment();
                 } else {
-                    plugin.getDeathCounter().decrement();
+                    plugin.getDeathCounter().set(plugin.getDeathCounter().get() - 1);
                 }
                 plugin.getDeathCounter().broadcastActionbar();
                 refreshMenu(type, p);
@@ -260,7 +260,6 @@ public class GuiListener implements Listener {
                     return;
                 }
 
-                EditType edit;
                 EditType edit = e.isRightClick() ? EditType.TOKEN : EditType.CHANNEL;
 
                 p.closeInventory();
